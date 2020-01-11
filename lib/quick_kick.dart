e6534e964b3264d59c53aa92f204d94b7f4008be
mario <mario.otmacic@gmail.com>
2020-01-12 00:56:41 +0100

    creating right side of screen

--- lib/quick_kick.dart
+++ lib/quick_kick.dart
@@ -96,122 +96,325 @@ class _QuickKickPageState extends State<QuickKickPage> {
       body: Container(
         child: Row(
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.fromLTRB(10.0, 20.0, 2.0, 2.0),
-              child: Container(
-                color: Color(0xFF2D2D3C),
-                width: 300.0,
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 80.0),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          IconButton(
-                              icon: Icon(
-                                Icons.arrow_back,
-                                size: 50.0,
-                                color: Color(0xFF9999AC),
-                              ),
-                              onPressed: () {
-                                Navigator.pop(context);
-                              }),
-                          SizedBox(
-                            width: 20.0,
-                          ),
-                          Flexible(
-                            child: Text(
-                              "QUICK KICK",
-                              style: TextStyle(
-                                  fontSize: 50.0,
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(10.0, 20.0, 2.0, 2.0),
+                child: Container(
+                  color: Color(0xFF2D2D3C),
+                  width: 300.0,
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 80.0),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            IconButton(
+                                icon: Icon(
+                                  Icons.arrow_back,
+                                  size: 50.0,
                                   color: Color(0xFF9999AC),
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.fromLTRB(10.0, 20.0, 15.0, 0.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Container(
-                            child: Text(
-                              "Select difficulty:",
-                              style: TextStyle(
-                                  color: Color(0xFF464655), fontSize: 18.0),
-                            ),
-                            margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
-                          ),
-                          Row(
-                            children: <Widget>[
-                              Expanded(
-                                child: Container(
-                                  padding:
-                                      EdgeInsets.fromLTRB(10.0, 7.0, 5.0, 7.0),
-                                  //color: Color(0xFF464655),
-                                  child: Container(child: _difficultyItems()),
-                                  decoration: BoxDecoration(boxShadow: [
-                                    BoxShadow(
-                                        color: Color(0xFF000000),
-                                        blurRadius: 5.0,
-                                        offset: Offset(5.0, 5.0))
-                                  ], color: Color(0xFF464655)),
                                 ),
-                              ),
-                              Container(
-                                height: 63.0,
-                                color: Color(0xFF242432),
-                                child: Icon(Icons.keyboard_arrow_down),
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    Expanded(
-                      child: Align(
-                        alignment: FractionalOffset.bottomRight,
-                        child: Padding(
-                          padding: const EdgeInsets.all(40.0),
-                          child: RaisedButton(
-                            padding: EdgeInsets.all(0.0),
-                            onPressed: null,
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  color: Color(0xFF3FA9F5),
-                                  boxShadow: [
-                                    BoxShadow(
-                                        color: Color(0xFF000000),
-                                        blurRadius: 8.0,
-                                        offset: Offset(7.0, 7.0))
-                                  ]),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                }),
+                            SizedBox(
+                              width: 20.0,
+                            ),
+                            Flexible(
                               child: Text(
-                                'START',
+                                "QUICK KICK",
                                 style: TextStyle(
-                                  fontSize: 30.0,
-                                  color: Color(0xFF282832),
-                                  fontWeight: FontWeight.bold,
-                                ),
+                                    fontSize: 50.0,
+                                    color: Color(0xFF9999AC),
+                                    fontWeight: FontWeight.bold),
                               ),
-                              padding: EdgeInsets.all(20.0),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(10.0, 20.0, 15.0, 0.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Container(
+                              child: Text(
+                                "Select difficulty:",
+                                style: TextStyle(
+                                    color: Color(0xFF464655), fontSize: 18.0),
+                              ),
+                              margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: Container(
+                                    padding: EdgeInsets.fromLTRB(
+                                        10.0, 7.0, 5.0, 7.0),
+                                    //color: Color(0xFF464655),
+                                    child: Container(child: _difficultyItems()),
+                                    decoration: BoxDecoration(boxShadow: [
+                                      BoxShadow(
+                                          color: Color(0xFF000000),
+                                          blurRadius: 5.0,
+                                          offset: Offset(5.0, 5.0))
+                                    ], color: Color(0xFF464655)),
+                                  ),
+                                ),
+                                Container(
+                                  height: 63.0,
+                                  color: Color(0xFF242432),
+                                  child: Icon(Icons.keyboard_arrow_down),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Align(
+                          alignment: FractionalOffset.bottomRight,
+                          child: Padding(
+                            padding: const EdgeInsets.all(40.0),
+                            child: RaisedButton(
+                              padding: EdgeInsets.all(0.0),
+                              onPressed: null,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: Color(0xFF3FA9F5),
+                                    boxShadow: [
+                                      BoxShadow(
+                                          color: Color(0xFF000000),
+                                          blurRadius: 8.0,
+                                          offset: Offset(7.0, 7.0))
+                                    ]),
+                                child: Text(
+                                  'START',
+                                  style: TextStyle(
+                                    fontSize: 30.0,
+                                    color: Color(0xFF282832),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                padding: EdgeInsets.all(20.0),
+                              ),
                             ),
                           ),
                         ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 4,
+              child: Container(
+                color: Color(0xFF242432),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0.0, 50.0, 0.0, 50.0),
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                              child: Column(
+                            children: <Widget>[
+                              Column(
+                                children: <Widget>[
+                                  Row(
+                                    children: <Widget>[
+                                      Text(
+                                        "Player height:",
+                                        style: TextStyle(
+                                            color: Color(0xFF464655),
+                                            fontSize: 20.0),
+                                      ),
+                                    ],
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                  ),
+                                  Row(
+                                    children: <Widget>[
+                                      Text(
+                                        "170-180cm",
+                                        style: TextStyle(fontSize: 20.0),
+                                      )
+                                    ],
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                  )
+                                ],
+                              ),
+                            ],
+                          )),
+                          Expanded(
+                              child: Column(
+                            children: <Widget>[
+                              Column(
+                                children: <Widget>[
+                                  Row(
+                                    children: <Widget>[
+                                      Text(
+                                        "Jump height:",
+                                        style: TextStyle(
+                                            color: Color(0xFF464655),
+                                            fontSize: 20.0),
+                                      )
+                                    ],
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                  ),
+                                  Row(
+                                    children: <Widget>[
+                                      Text(
+                                        "30-40 cm",
+                                        style: TextStyle(fontSize: 20.0),
+                                      )
+                                    ],
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                  )
+                                ],
+                              )
+                            ],
+                          )),
+                          Expanded(
+                              child: Column(
+                            children: <Widget>[
+                              Column(
+                                children: <Widget>[
+                                  Row(
+                                    children: <Widget>[
+                                      Text(
+                                        "Jump occurrence:",
+                                        style: TextStyle(
+                                            color: Color(0xFF464655),
+                                            fontSize: 20.0),
+                                      )
+                                    ],
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                  ),
+                                  Row(
+                                    children: <Widget>[
+                                      Text(
+                                        "60-70%",
+                                        style: TextStyle(fontSize: 20.0),
+                                      )
+                                    ],
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                  )
+                                ],
+                                mainAxisAlignment: MainAxisAlignment.center,
+                              )
+                            ],
+                          ))
+                        ],
                       ),
+                    ),
+                    SizedBox(
+                      height: 100.0,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                child: Row(
+                                  children: <Widget>[
+                                    Column(
+                                      children: <Widget>[
+                                        SizedBox(
+                                          height: 100,
+                                        ),
+                                      ],
+                                    ),
+                                    Column(
+                                      children: <Widget>[
+                                        Icon(Icons.battery_full)
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                          flex: 1,
+                        ),
+                        Expanded(
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                child: Image.asset(
+                                  "images/silueta-active.png",
+                                  fit: BoxFit.fill,
+                                  height: 400,
+                                ),
+                              )
+                            ],
+                          ),
+                          flex: 2,
+                        ),
+                        Expanded(
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                child: Image.asset(
+                                  "images/silueta-active.png",
+                                  fit: BoxFit.fill,
+                                  height: 400,
+                                ),
+                              )
+                            ],
+                          ),
+                          flex: 2,
+                        ),
+                        Expanded(
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                child: Image.asset(
+                                  "images/silueta-active.png",
+                                  fit: BoxFit.fill,
+                                  height: 400,
+                                ),
+                              )
+                            ],
+                          ),
+                          flex: 2,
+                        ),
+                        Expanded(
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                child: Image.asset(
+                                  "images/silueta-active.png",
+                                  fit: BoxFit.fill,
+                                  height: 400,
+                                ),
+                              )
+                            ],
+                          ),
+                          flex: 2,
+                        ),
+                        Expanded(
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                child: Image.asset(
+                                  "images/silueta-disabled.png",
+                                  fit: BoxFit.fill,
+                                  height: 400,
+                                ),
+                              )
+                            ],
+                          ),
+                          flex: 2,
+                        )
+                      ],
                     )
                   ],
                 ),
               ),
-            ),
-            Column(
-              children: <Widget>[
-                Row(
-                  children: <Widget>[Text("fdgjdfjfgdfjo")],
-                ),
-              ],
             )
           ],
         ),
