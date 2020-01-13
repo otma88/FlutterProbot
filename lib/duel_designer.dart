44622eb16c2849dc7af13ccdf4ab3a7f329e6c95
mario <mario.otmacic@gmail.com>
2020-01-13 23:29:16 +0100

    created duel designer dart file

---
+++ lib/duel_designer.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class DuelDesigner extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DuelDesignerPage(),
+    );
+  }
+}
+
+class DuelDesignerPage extends StatefulWidget {
+  @override
+  _DuelDesignerPageState createState() => _DuelDesignerPageState();
+}
+
+class _DuelDesignerPageState extends State<DuelDesignerPage> {
+  @override
+  Widget build(BuildContext context) {}
+}
